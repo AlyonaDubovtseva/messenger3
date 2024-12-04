75b949442514771809ff1758333c0ffdce1f01d3
elvinka505 <gazetdiinovaelvina@gmail.com>
2024-12-04 21:58:14 +0300

    commit from elvinka505

--- src/main/java/ru/message/Message.java
+++ src/main/java/ru/message/Message.java
@@ -1,5 +1,41 @@
 package ru.message;
 
 public class Message {
-    private String User;
+    private User sender;
+    private User recipient;
+    private String text;
+    private int date;
+    private int time;
+
+    public Message(User sender, User recipient, String text, int date, int time) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.text = text;
+        this.date = date;
+        this.time = time;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public String toString() {
+        return "[" + date + " " + time + "]" + sender.getNickname() + "to" + recipient.getNickname() + ":" + text;
+    }
 }
